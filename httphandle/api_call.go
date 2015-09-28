74d0effa5fbac6abdc151805a2863857b1c82894
Claudiu Codoban <codobanclaudiu@gmail.com>
2015-09-28 20:12:01 +0300

    Made some caching events run in a separate goroutine in order to improve response times in the API call module

--- httphandle/api_call.go
+++ httphandle/api_call.go
@@ -35,7 +35,7 @@ func PerformApiCall(handlerName string, rw http.ResponseWriter, req *http.Reques
 				GiveApiResponse(cachedData.StatusCode, cachedData.Data, rw, req, route.Pattern, cachedData.ContentType, cachedData.File)
 				return
 			} else { // Invalidate the cache if a modification, deletion or addition was made to this endpoint
-				cachedData.Invalidate()
+				go cachedData.Invalidate()
 			}
 		}
 	}
@@ -85,9 +85,11 @@ func respond(vars *api.ApiVar, resp *api.ApiResponse, rw http.ResponseWriter, re
 		GiveApiResponse(resp.StatusCode, resp.Message, rw, req, endpoint, resp.ContentType, resp.File)
 
 		// Try caching the data only if a GET request was made
-		if req.Method == api.GET && cache.Status == cache.STATUS_ON {
-			cacheResponse(vars, resp, endpoint)
-		}
+		go func(vars *api.ApiVar, resp *api.ApiResponse, req *http.Request, endpoint string) {
+			if req.Method == api.GET && cache.Status == cache.STATUS_ON {
+				cacheResponse(vars, resp, endpoint)
+			}
+		}(vars, resp, req, endpoint)
 	}
 }
 
