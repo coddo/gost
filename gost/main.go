e0d02ddc733a221fe251d8dceb4397aca67caa17
coddo <codobanclaudiu@gmail.com>
2016-03-17 22:43:36 +0200

    Added missing godoc and comments in the main (gost) package and also reordered the methods to have a more natura sequence in the file

--- gost/main.go
+++ gost/main.go
@@ -17,14 +17,22 @@ import (
 
 var numberOfProcessors = runtime.NumCPU()
 
+// APIContainer is a struct used for boxing all the existing api endpoints. It is used for mapping requests to functions.
 // Add all the existing endpoints as part of this container
-type ApiContainer struct {
+type APIContainer struct {
 	appuserapi.ApplicationUsersAPI
 	transactionapi.TransactionsAPI
 	userloginapi.UserSessionsAPI
 }
 
-func StartWebFramework() {
+// Application entry point - sets the behavior for the app
+func main() {
+	startWebFramework()
+}
+
+// startWebFramework performs the startup operations for the entire web framework
+// and starts the actual http or https server used for listening for requests.
+func startWebFramework() {
 	// Start listener for performing a graceful shutdown of the server
 	go listenForInterruptSignal()
 
@@ -38,7 +46,7 @@ func StartWebFramework() {
 
 // Function for performing automatic initializations at application startup
 func init() {
-	var emptyConfigParam string = ""
+	var emptyConfigParam string
 
 	// Initialize application configuration
 	config.InitApp(emptyConfigParam)
@@ -49,7 +57,7 @@ func init() {
 	service.InitDbService()
 
 	// Register the API endpoints
-	httphandle.SetApiInterface(new(ApiContainer))
+	httphandle.SetApiInterface(new(APIContainer))
 
 	// Start the caching system
 	cache.StartCachingSystem(cache.CacheExpireTime)
@@ -58,17 +66,13 @@ func init() {
 	runtime.GOMAXPROCS(numberOfProcessors)
 }
 
-// Application entry point - sets the behavior for the app
-func main() {
-	StartWebFramework()
-}
-
 func listenForInterruptSignal() {
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
 
 	<-signalChan
 
+	log.Println("")
 	log.Println("The server will now shut down gracefully...")
 
 	service.CloseDbService()
