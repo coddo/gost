6a63e0547f297d8a0ed283b18386914f654317c5
coddo <codobanclaudiu@gmail.com>
2016-03-23 19:39:49 +0200

    Finished implementing the database cookie store. Also improved the existing cookie store functionalities

---
+++ auth/sessions/dbstore.go
@@ -0,0 +1,92 @@
+package sessions
+
+import (
+	"gost/service"
+
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+// DatabaseCookieStore manages cookies using a database
+type DatabaseCookieStore struct {
+	location string
+}
+
+// ReadCookie fetches a cookie from the cookie store
+func (store *DatabaseCookieStore) ReadCookie(key string) (*Session, error) {
+	session, collection := service.Connect(store.location)
+	defer session.Close()
+
+	var cookie *Session
+	err := collection.Find(bson.M{"token": key}).One(cookie)
+
+	return cookie, err
+}
+
+// WriteCookie writes a cookie in the cookie store. If that cookie already exists,
+// it is overwritten
+func (store *DatabaseCookieStore) WriteCookie(cookie *Session) error {
+	session, collection := service.Connect(store.location)
+	defer session.Close()
+
+	err := collection.Update(bson.M{"token": cookie.Token}, cookie)
+	if err == mgo.ErrNotFound {
+		err = collection.Insert(cookie)
+	}
+
+	return err
+}
+
+// DeleteCookie deletes a cookie from the cookie storage
+func (store *DatabaseCookieStore) DeleteCookie(key string) error {
+	session, collection := service.Connect(store.location)
+	defer session.Close()
+
+	err := collection.Remove(bson.M{"token": key})
+	return err
+}
+
+// Init initializes the cookie store
+func (store *DatabaseCookieStore) Init() {
+	session, collection := service.Connect(store.location)
+	defer session.Close()
+	session.SetMode(mgo.Monotonic, true)
+
+	if store.hasTokenIndex(collection) {
+		return
+	}
+
+	index := mgo.Index{
+		Key: []string{"$text:token"},
+	}
+
+	err := collection.EnsureIndex(index)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// hasTokenIndex verifies if there is already an index created for the token field of a collection
+func (store *DatabaseCookieStore) hasTokenIndex(collection *mgo.Collection) bool {
+	indexes, err := collection.Indexes()
+	if err != nil {
+		panic(ErrInitializationFailed)
+	}
+
+	for _, index := range indexes {
+		for _, key := range index.Key {
+			if key == "token" {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+// NewDatabaseCookieStore creates a new DatabaseCookieStore pointer entity
+func NewDatabaseCookieStore(storeLocation string) *DatabaseCookieStore {
+	store := &DatabaseCookieStore{location: storeLocation}
+
+	return store
+}
---
+++ auth/sessions/filestore.go
@@ -0,0 +1,82 @@
+package sessions
+
+import (
+	"bytes"
+	"gost/util"
+	"io/ioutil"
+	"os"
+)
+
+// FileCookieStore manages cookies using files on the local drive
+type FileCookieStore struct {
+	location string
+}
+
+// ReadCookie fetches a cookie from the cookie store
+func (store *FileCookieStore) ReadCookie(key string) (*Session, error) {
+	fileName := store.fileLocation(key)
+	encodedData, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return nil, err
+	}
+
+	jsonData, err := util.Decode(encodedData)
+	if err != nil {
+		return nil, err
+	}
+
+	var session *Session
+	err = util.DeserializeJSON(jsonData, session)
+
+	return session, err
+}
+
+// WriteCookie writes a cookie in the cookie store. If that cookie already exists,
+// it is overwritten
+func (store *FileCookieStore) WriteCookie(cookie *Session) error {
+	fileName := store.fileLocation(cookie.Token)
+	jsonData, err := util.SerializeJSON(cookie)
+	if err != nil {
+		return err
+	}
+
+	encodedData := util.Encode(jsonData)
+
+	return ioutil.WriteFile(fileName, encodedData, os.ModeDevice)
+}
+
+// DeleteCookie deletes a cookie from the cookie storage
+func (store *FileCookieStore) DeleteCookie(key string) error {
+	fileName := store.fileLocation(key)
+
+	return os.Remove(fileName)
+}
+
+// Init initializes the cookie store
+func (store *FileCookieStore) Init() {
+	if _, err := os.Stat(store.location); os.IsNotExist(err) {
+		err = os.Mkdir(store.location, os.ModeDevice)
+
+		if err != nil {
+			panic(ErrInitializationFailed)
+		}
+	}
+}
+
+// cookieLocation computes and returns the location of a certain cookie
+func (store *FileCookieStore) fileLocation(key string) string {
+	var buffer bytes.Buffer
+
+	buffer.WriteString(store.location)
+	buffer.WriteRune('/')
+	buffer.WriteString(key)
+
+	return buffer.String()
+}
+
+// NewFileCookieStore creates a new NewFileCookieStore pointer entity
+func NewFileCookieStore(storeLocation string) *FileCookieStore {
+	store := &FileCookieStore{location: storeLocation}
+
+	return store
+}
