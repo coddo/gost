7be6b729635058230dd356bb81f9755db20ae804
coddo <codobanclaudiu@gmail.com>
2016-04-04 16:33:20 +0300

    Added endpoint method for developers to create app users

--- api/valuesapi/appusers.go
+++ api/valuesapi/appusers.go
@@ -1,2 +1,25 @@
 package valuesapi
 
+import (
+	"gost/api"
+	"gost/auth/identity"
+	"gost/util"
+	"net/http"
+)
+
+// CreateAppUser is an endpoint used for creating application users
+func (v *ValuesAPI) CreateAppUser(params *api.Request) api.Response {
+	user := &identity.ApplicationUser{}
+
+	err := util.DeserializeJSON(params.Body, user)
+	if err != nil {
+		return api.BadRequest(api.ErrEntityFormat)
+	}
+
+	err = identity.CreateUser(user)
+	if err != nil {
+		return api.InternalServerError(err)
+	}
+
+	return api.StatusResponse(http.StatusOK)
+}
