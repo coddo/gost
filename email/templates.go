11cc1c39805496e7ad0b6b53e2aec0b684c213ec
coddo <codobanclaudiu@gmail.com>
2016-04-20 23:10:42 +0300

    Properly styled the email templates

--- email/templates.go
+++ email/templates.go
@@ -1,21 +1,57 @@
 package email
 
-const activateAccountTemplate = `<html>
-<body>
-    <h1> Hello %s</h1> <br><br>
-    <h2> Welcome to the GOST web framework!</h2>
-    <br>
-    <br>
-    <p style="text-size: 20px">To activate your account, please use the following link: %s</p>
-</body>
-</html>`
+import "fmt"
 
-const resetPasswordTemplate = `<html>
+// ParseTemplate parses a standard HTML template and places the parameters in the
+// template's indicated parts
+func ParseTemplate(templateFormat string, params ...interface{}) string {
+	return fmt.Sprintf(templateFormat, params)
+}
+
+const (
+	activateAccountSubject  = `Welcome on board`
+	activateAccountTemplate = `
+<html>
+<head>
+    <style>
+    p {
+        font-size: 20px;
+    }
+    </style>
+</head>
 <body>
-    <h1> Hello %s</h1> <br><br>
-    <h2>A password reset was requested for this account. If it was not you who made the request, please disregard this email</h2>
-    <br>
-    <br>
-    <p style="text-size: 20px">To reset your password, please use the following link: %s</p>
+    <h2> Hello there,</h1> 
+    <h2> Welcome to the GOST web framework!</h2>
+    <br /> <br />
+    <p>To activate your account, please use the following link: %s</p>
+    <br /> <br />
+    <p> Cheers! </p>
+    <p> GOST Team </p>
 </body>
 </html>`
+)
+
+const (
+	resetPasswordSubject  = `Password reset`
+	resetPasswordTemplate = `
+<html>
+<head>
+    <style>
+    p {
+        font-size: 20px;
+    }
+    </style>
+</head>
+<body>
+    <h2> Hello there,</h1> <br/>
+    <h2>A password reset was requested for this account. <br/><br/>
+        If it was not you who made the request, please disregard this email
+    </h2>
+    <br /> <br />
+    <p>To reset your password, please use the following link: %s</p>
+    <br /> <br />
+    <p> Cheers! </p>
+    <p> GOST Team </p>
+</body>
+</html>`
+)
