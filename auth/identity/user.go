7cb52d4f13adb07220542293e613c32d4ca01818
coddo <codobanclaudiu@gmail.com>
2016-04-24 00:51:34 +0300

    Added new methods for fetching an ApplicationUser and refactored the file

--- auth/identity/user.go
+++ auth/identity/user.go
@@ -32,7 +32,7 @@ type ApplicationUser struct {
 	ResetPasswordTokenExpireDate   time.Time `bson:"resetPasswordTokenExpireDate,omitempty" json:"resetPasswordTokenExpireDate"`
 	ActivateAccountToken           string    `bson:"activateAccountToken" json:"activateAccountToken"`
 	ActivateAccountTokenExpireDate time.Time `bson:"activateAccountTokenExpireDate,omitempty" json:"activateAccountTokenExpireDate"`
-	AccountStatus                  int       `bson:"status,omitempty" json:"status"`
+	AccountStatus                  int       `bson:"accountStatus,omitempty" json:"accountStatus"`
 }
 
 // CreateUser adds a new ApplicationUser to the database
@@ -44,7 +44,7 @@ func CreateUser(user *ApplicationUser) error {
 		user.ID = bson.NewObjectId()
 	}
 
-	err := collection.Insert(user)
+	var err = collection.Insert(user)
 
 	return err
 }
@@ -58,7 +58,7 @@ func UpdateUser(user *ApplicationUser) error {
 		return service.ErrNoIDSpecified
 	}
 
-	err := collection.UpdateId(user.ID, user)
+	var err = collection.UpdateId(user.ID, user)
 
 	return err
 }
@@ -68,22 +68,52 @@ func GetUser(userID bson.ObjectId) (*ApplicationUser, error) {
 	session, collection := service.Connect(collectionName)
 	defer session.Close()
 
-	user := ApplicationUser{}
-	err := collection.FindId(userID).One(&user)
+	var user = ApplicationUser{}
+	var err = collection.FindId(userID).One(&user)
 
 	return &user, err
 }
 
+// GetUserByActivationToken retrieves an ApplicationUser from the database, based on its account activation token
+func GetUserByActivationToken(token string) (*ApplicationUser, error) {
+	session, collection := service.Connect(collectionName)
+	defer session.Close()
+
+	var user = ApplicationUser{}
+	var err = collection.Find(bson.M{"activateAccountToken": token}).One(&user)
+
+	return &user, err
+}
+
+// GetUserByResetPasswordToken retrieves an ApplicationUser from the database, based on its reset password token
+func GetUserByResetPasswordToken(token string) (*ApplicationUser, error) {
+	session, collection := service.Connect(collectionName)
+	defer session.Close()
+
+	var user = ApplicationUser{}
+	var err = collection.Find(bson.M{"resetPasswordToken": token}).One(&user)
+
+	return &user, err
+}
+
+// DeleteUser deletes an ApplicationUser from the database, based on its ID
+func DeleteUser(userID bson.ObjectId) error {
+	session, collection := service.Connect(collectionName)
+	defer session.Close()
+
+	return collection.RemoveId(userID)
+}
+
 // IsUserExistent verifies if an user with the given id exists
 func IsUserExistent(userID bson.ObjectId) bool {
-	user, err := GetUser(userID)
+	var user, err = GetUser(userID)
 
 	return err == nil && user != nil
 }
 
 // IsUserActivated verifies if an user account is activated
 func IsUserActivated(userID bson.ObjectId) bool {
-	user, err := GetUser(userID)
+	var user, err = GetUser(userID)
 
 	return err == nil && user.AccountStatus == AccountStatusActivated
 }
